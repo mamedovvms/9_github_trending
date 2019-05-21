fe6e37eb4e3d3105d2e40226b0d6ec69042f597f
1237 <mamedov_vs@oksw.ru>
2019-05-21 20:09:29 +0700

    Добавлена функция получения открытых проблем.

--- github_trending.py
+++ github_trending.py
@@ -2,44 +2,74 @@ from datetime import datetime, timedelta
 import requests
 
 
-URL_SEARCH_REPOSITORIES = 'https://api.github.com/search/repositories'
-TIME_DELTA = 7
-TOP_SIZE = 20
-
-
-def get_params_request():
-    time_start = datetime.utcnow().date() - timedelta(days=TIME_DELTA)
+def get_request_params(time_delta, top_size):
+    time_start = datetime.utcnow().date() - timedelta(days=time_delta)
     time_start_str = time_start.strftime('%Y-%m-%d')
     return {
         'q': 'created:>{}'.format(time_start_str),
         'sort': 'stars',
-        'per_page': TOP_SIZE
+        'per_page': top_size
     }
 
 
-def get_trending_repositories():
-    params = get_params_request()
-    response = requests.get(URL_SEARCH_REPOSITORIES, params)
+def get_trending_repositories(url, time_delta, top_size):
+    params = get_request_params(time_delta, top_size)
+    response = requests.get(url, params)
     return response.json()['items']
 
 
+def get_open_issues(repo_owner, repo_name):
+    url_template = 'https://api.github.com/repos/{}/{}/issues'.format(
+        repo_owner,
+        repo_name
+    )
+    params = {
+        'state': 'open',
+        'per_page': '100',
+    }
+    page = 0
+    while True:
+        page += 1
+        params['page'] = page
+        response = requests.get(url_template, params=params)
+        issues = response.json()
+        if issues:
+            yield from issues
+        else:
+            return False
+
+
 def get_open_issues_amount(repo_owner, repo_name):
-    pass
+    issues_amount = 0
+    for issue in get_open_issues(repo_owner, repo_name):
+        if 'id' in issue and not 'pull_request' in issue:
+            issues_amount += 1
+    return issues_amount
 
 
 def main():
+    url_search_repositories = 'https://api.github.com/search/repositories'
+    time_delta = 7
+    top_size = 20
+
     template_response = '''
     Name: {}
     URL: {}
     Stars: {}
     Open issues: {}
     '''
-    trending_repositories = get_trending_repositories()
+    trending_repositories = get_trending_repositories(
+        url_search_repositories,
+        time_delta,
+        top_size
+    )
     for repo in trending_repositories:
-        print(template_response.format(repo['name'],
-                                       repo['html_url'],
-                                       repo['stargazers_count'],
-                                       repo['open_issues_count']))
+        print(template_response.format(
+            repo['name'],
+            repo['html_url'],
+            repo['stargazers_count'],
+            get_open_issues_amount(repo['owner']['login'], repo['name'])
+        ))
 
 
 if __name__ == '__main__':
